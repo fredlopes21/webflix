8d0c482c117174b6379c74d4e925531b524c5e70
maxwelloniate <maxwelloniate@gmail.com>
2020-01-24 12:01:15 -0500

    test

--- includes/config.php
+++ includes/config.php
@@ -8,7 +8,7 @@ session_start();
 date_default_timezone_set('America/New_York');
 
 try {
-  $con = new PDO('mysql:dbname=webflix;host=localhost', 'root', 'pass');
+  $con = new PDO('mysql:dbname=heroku_08f1b312e9fb135;host=us-cdbr-iron-east-05.cleardb.net', '	b0ebce749ed39a', '8f9167a9');
   $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 } catch (PDOException $e) {
   exit('Connection failed: ' . $e->getMessage());
